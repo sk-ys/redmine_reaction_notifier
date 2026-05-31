1f46163d53577619fd8653f2c3671ecbe69dcc6e
sk-ys <yskrmt@gmail.com>
2026-05-31 10:28:22 +0900

    fix: stabilize Redmine 6.1 dev reload for reaction notifier patches and hooks

--- init.rb
+++ init.rb
@@ -1,6 +1,26 @@
-require_relative 'lib/redmine_reaction_notifier/reaction_patch'
-require_relative 'lib/redmine_reaction_notifier/hooks'
-require_relative 'lib/redmine_reaction_notifier/user_preference_patch'
+if Rails.env.production?
+  require_relative 'lib/redmine_reaction_notifier/reaction_patch'
+  require_relative 'lib/redmine_reaction_notifier/user_preference_patch'
+  require_relative 'lib/redmine_reaction_notifier/hooks'
+else
+  unless defined?(RedmineReactionNotifier::ReactionPatch)
+    load File.expand_path('lib/redmine_reaction_notifier/reaction_patch.rb', __dir__)
+  end
+  unless defined?(RedmineReactionNotifier::UserPreferencePatch)
+    load File.expand_path('lib/redmine_reaction_notifier/user_preference_patch.rb', __dir__)
+  end
+
+  # In development, remove existing listeners before reloading hooks to avoid duplicate registrations.
+  begin
+    listener_classes = Redmine::Hook.class_variable_get(:@@listener_classes)
+    removed = listener_classes.reject! { |klass| klass.name == 'RedmineReactionNotifier::Hooks' }
+    Redmine::Hook.clear_listeners_instances if removed
+  rescue NameError
+    # Ignore when hook internals are not initialized yet.
+  end
+  RedmineReactionNotifier.send(:remove_const, :Hooks) if defined?(RedmineReactionNotifier::Hooks)
+  load File.expand_path('lib/redmine_reaction_notifier/hooks.rb', __dir__)
+end
 
 Redmine::Plugin.register :redmine_reaction_notifier do
   name 'Redmine Reaction Notifier'
@@ -12,11 +32,9 @@ Redmine::Plugin.register :redmine_reaction_notifier do
   requires_redmine version_or_higher: '6.1.0'
 end
 
-Rails.configuration.after_initialize do
-  unless Reaction.included_modules.include?(RedmineReactionNotifier::ReactionPatch)
-    Reaction.include(RedmineReactionNotifier::ReactionPatch)
-  end
-  unless UserPreference.included_modules.include?(RedmineReactionNotifier::UserPreferencePatch)
-    UserPreference.include(RedmineReactionNotifier::UserPreferencePatch)
-  end
+unless Reaction.included_modules.include?(RedmineReactionNotifier::ReactionPatch)
+  Reaction.include(RedmineReactionNotifier::ReactionPatch)
+end
+unless UserPreference.included_modules.include?(RedmineReactionNotifier::UserPreferencePatch)
+  UserPreference.include(RedmineReactionNotifier::UserPreferencePatch)
 end
